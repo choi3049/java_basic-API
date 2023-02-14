3b8bd72ead31557858064a51fe771105325fce1a
choe <qudtjs3012@naver.com>
2023-02-14 23:42:35 +0900

    API-object

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import static org.example.objectTest.*;
+
+public class Main {
+    public static void main(String[] args) {
+        eq(); // equals
+        ts(); // toString
+    }
+}
---
+++ src/main/java/org/example/objectTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class objectTest {
+    static String a = "abs";
+    static String b = "abs";
+    static Object oe = new Object();
+    public static void eq(){
+        System.out.println(a.equals(b));
+    }
+
+    public static void ts(){
+        System.out.println(oe.toString());
+    }
+}
+
